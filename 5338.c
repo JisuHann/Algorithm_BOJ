12e98a683a564c7c9919da4a7a1dba95b0be7cd3
Jisu_Han <jshcdi6658@gmail.com>
2019-10-05 20:48:54 +0900

    Create 5338.c

---
+++ 5338.c
@@ -0,0 +1,12 @@
+//백준 5338: https://www.acmicpc.net/problem/5338
+
+#include <stdio.h>
+
+int main() {
+	printf("       _.-;;-._\n");
+	printf("'-..-'|   ||   |\n");
+	printf("'-..-'|_.-;;-._|\n");
+	printf("'-..-'|   ||   |\n");
+	printf("'-..-'|_.-''-._|\n");
+  return 0;
+}
