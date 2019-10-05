157e115756b9cdf3393e039a79e64646e6078501
Jisu_Han <jshcdi6658@gmail.com>
2019-10-05 22:19:22 +0900

    Create 15740.java

---
+++ 15740.java
@@ -0,0 +1,17 @@
+# problem code: https://www.acmicpc.net/problem/15740
+
+import java.util.*;
+import java.io.*;
+import java.math.BigInteger;
+
+public class plus {
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+            BigInteger a = sc.nextBigInteger();
+            BigInteger b = sc.nextBigInteger();
+            System.out.println(a.add(b));
+        }
+}
+
