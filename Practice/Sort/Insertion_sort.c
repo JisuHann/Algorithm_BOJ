3e2d49455015fe32291f0aa51910394be6031f38
Jisu Han <jshcdi6658@gmail.com>
2019-12-27 11:55:50 +0900

    Create Insertion_sort.c

---
+++ Practice/Sort/Insertion_sort.c
@@ -0,0 +1,11 @@
+insertion_sort(A, n);
+
+void insertion_sort(int list[], int n){
+  int i, j, key;
+  for(int i = 1;i<n;i++){
+    key = list[i];
+    for(j = i-1;j>=0 && list[j]>key;j--)
+      list[j+1]=list[j];
+    list[j+1]=key;
+  }
+}
