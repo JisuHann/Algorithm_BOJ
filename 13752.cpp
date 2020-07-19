4dea523ecca08b57023317b1778de2d3da7feeea
Jisu Han <jshcdi6658@gmail.com>
2020-07-19 10:27:16 +0900

    Create 13752.cpp

---
+++ 13752.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string>
+#include <string.h>
+#include <vector>
+#include <stdlib.h>
+#include <iostream>
+#include <algorithm>
+#pragma warning(disable:4996)                                                                      
+#define _CRT_SECURE_NO_WARNINGS
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		int k;
+		cin >> k;
+		for (int l = 0; l < k; l++) {
+			cout << "=";
+		}
+		cout << "\n";
+	}
+
+	return 0;
+}
