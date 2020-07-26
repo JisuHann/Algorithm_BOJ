d69bbb7e81d8509fb156b633f6af8b3161feee56
Jisu Han <jshcdi6658@gmail.com>
2020-07-26 09:01:11 +0900

    Create 1927.cpp

---
+++ 1927.cpp
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <string.h>
+#include <string>
+#include <stdlib.h>
+#include <iostream>
+#include <algorithm>
+#include <math.h>
+#include <vector>
+#include <queue>
+#include <functional>
+#pragma warning(disable:4996)
+#define _CRT_SECURE_NO_WARNINGS
+using namespace std;
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	priority_queue<int, vector<int>, greater<int> > a;
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		int tmp;
+		cin >> tmp;
+		if (tmp == 0) {
+			if (a.size()) {
+				printf("%d\n", a.top());
+				a.pop();
+			}
+			else {
+				printf("0\n");
+			}
+
+		}
+		else {
+			a.push(tmp);
+
+		}
+	}
+
+	return 0;
+}
