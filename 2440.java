339485560ad03448a51140d2e31b79da39fea763
Jisu Han <han_js@ewhain.net>
2021-01-20 07:34:15 +0900

    Create 2440.java

---
+++ 2440.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		
+		for(int i=num;i>=0;i--) {
+			for(int l=1;l<=i;l++) {
+				System.out.print("*");
+			}
+			for(int k=num-i;k>=1;k--) {
+				System.out.print(" ");
+			}
+			System.out.print("\n");
+		}
+				
+		sc.close();
+	}
+}
