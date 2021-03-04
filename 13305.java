af48c74bf995b47b740d9fab3db873645d0ec589
Jisu Han <han_js@ewhain.net>
2021-03-04 21:16:30 +0900

    Create 13305.java

---
+++ 13305.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    int N = in.nextInt();
+    long[] dist = new long[N - 1]; 
+    long[] cost = new long[N];
+
+    for(int i = 0; i < N - 1; i++) {dist[i] = in.nextLong();}    
+    for(int i = 0; i < N; i++) {cost[i] = in.nextLong();}
+    long sum = 0;
+    long minCost = cost[0];
+    for(int i = 0; i < N - 1; i++) {
+        if(cost[i] < minCost) {minCost = cost[i];}
+        sum += (minCost * dist[i]);
+    }
+    System.out.println(sum);
+    }
+}
+
